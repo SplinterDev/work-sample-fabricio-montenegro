8e97d3abe5eb835f14098b1672b7166e105bfabb
Fabrício Montenegro <fabriciojcmontenegro@gmail.com>
2020-12-12 17:16:56 -0300

    Show list of movies

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import MoviesList from './containers/MoviesList/MoviesList';
 
-const ENDPOINT = 'https://us-central1-beacon-fe-worksample-api.cloudfunctions.net/app';
-
-
 function App() {
-
-  const [movies, setMovies] = useState([]);
-
-  useEffect(async () => {
-
-    const movies = localStorage.getItem('movies');
-
-    if (movies) {
-      setMovies(movies);
-    } else {
-      fetch(`${ENDPOINT}/movies`)
-        .then(res => res.json())
-        .then(res => {
-          localStorage.setItem('movies', res);
-          setMovies(res);
-        })
-        .catch(err => console.error(err));
-    }
-
-    return () => {
-      // cleanup
-    }
-  }, ['input'])
-
   return (
     <div className="App">
       <header>
--- src/containers/MoviesList/MoviesList.js
+++ src/containers/MoviesList/MoviesList.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const ENDPOINT = 'https://us-central1-beacon-fe-worksample-api.cloudfunctions.net/app';
 
 function MoviesList() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const movies = JSON.parse(localStorage.getItem('movies'));
+
+    if (movies) {
+      setMovies(movies);
+    } else {
+      fetch(`${ENDPOINT}/movies`)
+        .then(res => res.json())
+        .then(res => {
+          localStorage.setItem('movies', JSON.stringify(res));
+          setMovies(res);
+        })
+        .catch(err => console.error(err));
+    }
+
+    return () => {
+      // cleanup
+    }
+  }, ['input'])
+
+console.log(movies);
+
   return (
     <ul>
-      <li>an item</li>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
     </ul>
   )
 }
